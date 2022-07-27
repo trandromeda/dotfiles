eace7a60db364a3f9e19082a1a7bc2dddd1a3e3c
Ravi Dayabhai <ravi.dayabhai@chime.com>
2022-07-26 23:23:22 -0700

    Update `Brewfile`

--- .global_ycm_extra_config.py
+++
@@ -1,9 +0,0 @@
-def Settings(**kwargs):
-    client_data = kwargs["client_data"]
-    return {
-        "interpreter_path": client_data["g:ycm_python_interpreter_path"],
-        "sys_path": client_data["g:ycm_python_sys_path"],
-        # Trivial set of flags if no compilation database found for C-language
-        # projects
-        "flags": ["-x", "c++", "-Wall", "-Wextra", "-Werror"],
-    }
