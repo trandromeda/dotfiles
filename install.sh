b0128ca4a0f3e76bba594f7f741dc40fffa5adf9
Andy Tran <transient.tran@gmail.com>
2023-02-22 17:58:26 -0500

    fix: update errors in script

--- install.sh
+++ install.sh
@@ -74,13 +74,19 @@ link_dotfiles() {
     fi
 }
 
-install_homebrew() {
+install_homebrew_and_xcode() {
     _process "→ Installing Homebrew"
+
+    # This step may take a while
     xcode-select --install
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
     _process "→ Running brew doctor"
     brew doctor
+
+    # Add Homebrew to the PATH
+    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> "${HOME}/.zprofile"
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 
     [[ $? ]] \
     && _success "Installed Homebrew"
@@ -124,11 +130,14 @@ manual_preadjustments() {
 
   _process "→ Creating directory at ${HOME}/.nvm"
   mkdir -p ${HOME}/.nvm
+  
+  _process "→ Creating directory for dotfiles "
+  mkdir -p ${HOME}/dev/${GITHUB_REPO}
 
   # Oh-My-Zsh 
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-  [[ $? ]] && _success "Manual adjustments made successfully"
+  [[ $? ]] && _success "Pre-setup tasks complete"
 }
 
 update_macos_settings() {
@@ -144,9 +153,9 @@ update_macos_settings() {
 }
 
 install() {
+  install_homebrew_and_xcode
   download_dotfiles
   manual_preadjustments
-  install_homebrew
   install_formulae
   link_dotfiles
   install_custom
