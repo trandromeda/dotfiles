2a8e6dc6ee39264c8cc3445c5756dde01af6024b
Andy Tran <transient.tran@gmail.com>
2023-02-22 18:01:45 -0500

    fix: add zsh-vi-mode to omz instead

--- install.sh
+++ install.sh
@@ -118,6 +118,7 @@ install_custom() {
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
   git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
+  git clone https://github.com/jeffreytse/zsh-vi-mode ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-vi-mode
   git clone https://github.com/tmux-plugins/tpm ${HOME}/.tmux/plugins/tpm && chmod 755 ${HOME}/.tmux
 
   [[ $? ]] && _success "All custom code installed"
